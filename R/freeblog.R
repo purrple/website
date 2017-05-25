2918809cf43fd8bc60443ad25cea51e42aef2bc7
Romain Francois <romain@thinkr.fr>
2017-05-25 16:45:25 +0200

    starting to rvest old posts from freeblog

---
+++ R/freeblog.R
@@ -0,0 +1,49 @@
+library(httr)
+library(rvest)
+library(purrr)
+library(stringr)
+library(dplyr)
+
+get_links <- function(page = 1){
+  url <- paste0("http://romainfrancois.blog.free.fr/index.php?/page/", page)  
+  url %>% 
+    read_html() %>% 
+    html_nodes( ".post-title a" ) %>% 
+    html_attr("href") %>% 
+    str_replace("^.*[?]post/", "") %>% 
+    data_frame(url=.) %>% 
+    separate( url, into = c("year", "month", "day", "slug" ), sep =  "/", extra = "merge", remove = FALSE)
+}
+links <- bind_rows( map(1:7, get_links) )
+
+download_post <- function(url){
+  prefix <- "http://romainfrancois.blog.free.fr/index.php?post/"
+  url <- paste0(prefix, url)
+  html <- read_html(url)
+  title <- html %>% 
+    html_node("h2.post-title") %>% 
+    html_text()
+  
+  tags <- html %>% 
+    html_nodes("ul.post-tags li a") %>% 
+    html_text()
+  
+  content <- html %>% 
+    html_node("div.post-content") %>% 
+    as.character()
+  
+  imgs <- html %>% 
+    html_nodes("div.post-content img") %>% 
+    html_attr("src")
+  
+  data_frame( 
+    title = title, 
+    tags = list(tags), 
+    content = content, 
+    imgs = list(imgs)
+    )
+}
+
+data <- links$url %>% 
+  map(download_post) %>% 
+  bind_rows()
